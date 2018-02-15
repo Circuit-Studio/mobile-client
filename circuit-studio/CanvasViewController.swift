a537d570f68da2c80398aa82be985edef72237b1
LinnierGames <esericksanc@gmail.com>
2018-02-15 11:26:34 -0800

    init view controller

---
+++ circuit-studio/CanvasViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CanvasViewController.swift
+//  circuit-studio
+//
+//  Created by Erick Sanchez on 2/15/18.
+//  Copyright © 2018 Circuit Studio. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+    
+    // MARK: - RETURN VALUES
+    
+    // MARK: - VOID METHODS
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
+
+}
