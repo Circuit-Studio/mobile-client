fde327137347f3556479be9c52d519321825e544
LinnierGames <esericksanc@gmail.com>
2018-02-15 13:35:06 -0800

    layout canvas view

--- circuit-studio/CanvasViewController.swift
+++ circuit-studio/CanvasViewController.swift
@@ -10,14 +10,58 @@ import UIKit
 
 class CanvasViewController: UIViewController {
     
+    @IBOutlet weak var labelDocTitle: UILabel!
+    @IBOutlet weak var containerComponentToolbar: UIView!
+    private var componentToolbarCollectionViewController: ComponentsToolbarCollectionViewController!
+    
     private var viewModel = CanvasViewModel()
     
     // MARK: - RETURN VALUES
     
     // MARK: - VOID METHODS
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "embeded collection controller":
+                guard let collectionViewController = segue.destination as? ComponentsToolbarCollectionViewController else {
+                    fatalError("ComponentsToolbarCollectionViewController is not set in the storyboard")
+                }
+                
+                componentToolbarCollectionViewController = collectionViewController
+            default: break
+            }
+        }
+    }
+    
     // MARK: - IBACTIONS
     
+    @IBAction func pressRenameDoc(_ sender: Any) {
+        
+    }
+    @IBAction func pressDocInfo(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var buttonProfile: UIButton!
+    @IBAction func pressUserProfile(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var segComponentsTab: UISegmentedControl!
+    @IBAction func segComponentsTabDidChange(_ sender: Any) {
+        
+    }
+    @IBAction func pressWireTool(_ sender: Any) {
+        
+    }
+    @IBAction func pressSelectTool(_ sender: Any) {
+        
+    }
+    @IBAction func pressTextboxTool(_ sender: Any) {
+        
+    }
+    @IBAction func pressShapeTool(_ sender: Any) {
+        
+    }
     // MARK: - LIFE CYCLE
 
 }
